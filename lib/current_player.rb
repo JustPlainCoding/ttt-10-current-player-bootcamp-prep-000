b80276708687ef9a98165754f5c6c5518840151b
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-27 19:43:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,8 @@ def turn_count(board)
   counter = 0
   board.each do |space|
     puts space
-    if space == "X" || space = "O"
-    counter += 1
+    if space == "X" || space == "O"
+      counter += 1
     puts counter
     else
     end
